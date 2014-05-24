9a4c8fc2f884c32dd502acfabadaef8a4bf72818
Tad Hardesty <tad@platymuus.com>
2014-05-23 21:50:52 -0500

    Kanji and Word pages

--- japandb/appmain.py
+++ japandb/appmain.py
@@ -3,16 +3,38 @@
 
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
-with open('data/test2.json') as f:
+with open('data/test3.json') as f:
     data = json.load(f)
 
 app = Flask(__name__)
 
+@app.context_processor
+def inject_python():
+    return dict(len=len)
+
 @app.route('/')
 def hello():
-    return render_template('index.html', data=data)
+    kanji_info = sorted(data['kanji'].items())
+    word_keys = sorted(data['words'].keys())
+    return render_template('index.html', kanji_items=kanji_info, word_keys=word_keys)
+
+@app.route('/kanji/<kanji>')
+def show_kanji(kanji):
+    if kanji not in data['kanji']:
+        return redirect('/')
+    
+    kanji_info = data['kanji'][kanji]
+    return render_template('kanji.html', kanji=kanji, info=kanji_info)
+
+@app.route('/word/<word>')
+def show_word(word):
+    if word not in data['words']:
+        return redirect('/')
+    
+    word_info = data['words'][word]
+    return render_template('word.html', word=word, info=word_info)
 
 @app.route('/flerp')
 def flerp():
