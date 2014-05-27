a65942f708df8a3436cb5c7fab8c9075dfcfd1a6
Tad Hardesty <tad@platymuus.com>
2014-05-26 23:25:37 -0500

    Updated to latest data format and added notes

--- japandb/data.py
+++ japandb/data.py
@@ -2,19 +2,32 @@
 # module responsible for loading & indexing data
 
 import json
+from collections import defaultdict
 
 # Perform initialization
 
-with open('data/test3.json') as f:
-    _data = json.load(f)
-    
-_words = _data["words"]
-_kanji = _data["kanji"]
+with open('data/output.json') as f:
+    _categories = json.load(f)
 
-words_by_kanji = {}
-for word, word_info in _words.items():
-    for kanji in word_info['kanji']:
-        words_by_kanji.setdefault(kanji, []).append(word)
+# add "category" to each word, make _words_by_category dict
+_words_by_category = {}
+_all_words = {}
+for category, cat_info in _categories.items():
+    _words_by_category[category] = cat_info["words"]
+    for word, word_info in cat_info["words"].items():
+        word_info["category"] = category
+        _all_words[word] = word_info
+
+# index the kanji in the words
+def _make_kanji_default():
+    return {'words': [], 'readings': {}}
+_kanji = defaultdict(_make_kanji_default)
+
+for word, word_info in _all_words.items():
+    for kanji_str in word_info["kanji"]:
+        for kanji in kanji_str:
+            _kanji[kanji]['words'].append(word)
+    # todo: determine readings properly
 
 # Public interface
 
@@ -30,7 +43,7 @@ def get_kanji_info_and_words(kanji):
     return _kanji[kanji], words_by_kanji.get(kanji, [])
 
 def get_word_keys():
-    return sorted(_words.keys())
+    return sorted(_all_words.keys())
     
 def get_word_info(word):
-    return _words.get(word, None)
+    return _all_words.get(word)
