ae1dbf316dd4722b352741e11d9c221e9990889e
SebastienGllmt <SebastienGllmt@gmail.com>
2014-12-04 10:42:22 -0800

    Similar change for apple icon

--- japandb/__main__.py
+++ japandb/__main__.py
@@ -3,3 +3,4 @@ from japandb.appmain import app
 app.run(debug=False, host='0.0.0.0')
 
 app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
+app.add_url_rule('/apple-touch-icon.png', redirect_to=url_for('static', filename='apple-touch-icon.png'))
